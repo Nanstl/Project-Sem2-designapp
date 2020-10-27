25299c2256f9e957bcb9e8eed86e0265da8855db
Nan Suthaloung <71818397+devnan001626@users.noreply.github.com>
2020-10-27 09:09:31 +0700

    Add files via upload
    step by step
    1.npm install
    2.ionic cordova plugin add cordova-plugin-firebase
    3.npm install firebase@7.11.0 @angular/fire@5.4.2 --save

---
+++ Sem2/src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'sign-in',
+    loadChildren: () => import('./sign-in/sign-in.module').then( m => m.SignInPageModule)
+  },
+  {
+    path: 'sign-up',
+    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
+  },  
+  {
+    path: 'appnote',
+    loadChildren: () => import('./appnote/appnote.module').then( m => m.AppnotePageModule)
+  },  {
+    path: 'detail',
+    loadChildren: () => import('./detail/detail.module').then( m => m.DetailPageModule)
+  },
+
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
